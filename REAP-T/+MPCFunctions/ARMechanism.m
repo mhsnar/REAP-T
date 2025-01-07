f6c3750c4f680e24732950c0d0177c3f58b53937
Mohsen Amiri <mohsen.amiri@wsu.edu>
2025-01-07 10:00:41 -0800

    Delete REAP-T/+MPCFunctions directory

--- REAP-T/+MPCFunctions/ARMechanism.m
+++
@@ -1,9 +0,0 @@
-function [hat_u,hatLambda] = ARMechanism(sigma_hat_u,sigma_hat_u0,hat_u0,hat_u,hatLambda0,hatLambda)
- if sigma_hat_u<=sigma_hat_u0
-        hatLambda=hatLambda;
-        hat_u=hat_u;
-    else
-        hatLambda=hatLambda0;
-        hat_u=hat_u0;
-    end
-end
